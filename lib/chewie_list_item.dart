cf4fdaad348c0b1cd0b7bfdad8570dffba57775e
Douglas Ceron <58908752+DouglasCeron@users.noreply.github.com>
2022-01-26 10:52:43 -0300

    implementação do chewie

---
+++ lib/chewie_list_item.dart
@@ -0,0 +1,123 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+
+class ChewieListItem extends StatefulWidget {
+  final VideoPlayerController? videoPlayerController;
+
+  final bool? loop;
+
+  ChewieListItem({
+    @required this.videoPlayerController,
+    this.loop,
+  });
+
+  @override
+  _ChewieListItemState createState() => _ChewieListItemState();
+}
+
+class _ChewieListItemState extends State<ChewieListItem> {
+  ChewieController? _chewieController;
+  bool isPlaying = false;
+  Widget? image = Image.asset(
+    'assets/hubla_img.jpeg',
+    height: double.infinity,
+    width: double.infinity,
+    fit: BoxFit.fill,
+  );
+  @override
+  void initState() {
+    videoListener();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    var prop = height / width;
+
+    return Column(
+      children: [
+        Container(
+          color: Colors.black,
+          padding: EdgeInsets.symmetric(vertical: 8 * prop),
+          height: height * 0.3,
+          child: Chewie(
+            controller: chewieController(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  chewieController() {
+    return _chewieController = ChewieController(
+      deviceOrientationsAfterFullScreen: [DeviceOrientation.portraitUp],
+      deviceOrientationsOnEnterFullScreen: [DeviceOrientation.landscapeLeft],
+      videoPlayerController: widget.videoPlayerController!,
+      overlay: getOverlay(),
+      aspectRatio: 1 / 1,
+      looping: widget.loop!,
+      autoInitialize: true,
+      showControls: true,
+      showControlsOnInitialize: false,
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Text(errorMessage,
+              style: const TextStyle(
+                color: Colors.white,
+              )),
+        );
+      },
+    );
+  }
+
+  void videoListener() {
+    widget.videoPlayerController!.addListener(checkVideo);
+  }
+
+  void checkVideo() {
+    // Implement your calls inside these conditions' bodies :
+
+    int? secondsPosition = widget.videoPlayerController?.value.position.inSeconds;
+    int? secondsDuration = widget.videoPlayerController!.value.duration.inSeconds;
+    int? minutesPosition = widget.videoPlayerController!.value.position.inMinutes;
+    Duration? actualPosition = widget.videoPlayerController?.value.position;
+    Duration? videoDuration = widget.videoPlayerController?.value.duration;
+
+    if (actualPosition == const Duration(seconds: 0, minutes: 0, hours: 0)) {
+      print('video Started');
+    }
+    if (widget.videoPlayerController!.value.isPlaying) {
+      setState(() {
+        print('playing setState');
+
+        isPlaying = true;
+      });
+    }
+    if (secondsPosition! <= secondsDuration) {
+      print('minutes:$minutesPosition, seconds:$secondsPosition');
+    }
+    if (actualPosition == videoDuration) {
+      print('video Ended');
+    }
+  }
+
+  Widget? getOverlay() {
+    if (isPlaying) {
+      print('playing');
+      return Container();
+    } else {
+      return image;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    widget.videoPlayerController?.dispose();
+    _chewieController?.dispose();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:poc_video_player/chewie_list_item.dart';
+import 'package:video_player/video_player.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  String url = 'https://stream.mux.com/Zub00MY8X01YIMm9UEf4Oe8R6Wd4wGsZu2fU7B7qx3iD4.m3u8';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video Player'),
+      ),
+      body: Column(
+        children: [
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset('assets/vd_teste_yt.mp4'),
+            loop: true,
+          ),
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.network(url),
+            loop: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
